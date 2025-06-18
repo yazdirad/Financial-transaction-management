fa0bb356af8243a7b75b6e24fd1625026a26a808
yazdirad <mahdi.y@gmail.com>
2025-06-18 20:37:59 +0430

    add font awesome and fix modal close btn

--- app/models/enums/categoriesType.js
+++ app/models/enums/categoriesType.js
@@ -1,7 +1,7 @@
 const categoriesType = {
-    Receipt: { id: 1, title: "درآمد" },
-    Expense: { id: 2, title: "هزینه" },
-    Both: { id: 3, title: "درآمد/هزینه" }
+    Receipt: { id: 1, title: "دریافتی" },
+    Expense: { id: 2, title: "پرداختی" },
+    Both: { id: 3, title: "دریافتی/پرداختی" }
 }
 
 module.exports = Object.freeze(categoriesType);
